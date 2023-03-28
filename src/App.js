9f194d0037151a21bdf6224caf59456470f66395
Mohamed Medhat Ali <medha.hamed96@gmail.com>
2023-03-28 15:11:25 +0200

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          3azanam
+          3azanam awy
         </p>
         <a
           className="App-link"
